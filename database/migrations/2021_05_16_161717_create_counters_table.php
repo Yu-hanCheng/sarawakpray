aa55537d45139038f1c9be44afeed7435aa0a0bd
Yu-hanCheng <sarahcheng1231@gmail.com>
2021-07-08 07:35:48 +0800

    [FIX] add migration counters

---
+++ database/migrations/2021_05_16_161717_create_counters_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('counters', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('tan');
+            $table->integer('book1');
+            $table->integer('book2');
+            $table->integer('book3');
+            $table->integer('veg');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('counters');
+    }
+}
